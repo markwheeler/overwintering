ba1a1f60d0f3b73566c5d8161d56867bc763cd4d
Mark Wheeler <mark@markwheeler.net>
2021-04-22 17:40:45 -0700

    first pass getting bird data on screen

--- lib/trove.lua
+++ lib/trove.lua
@@ -1,17 +1,84 @@
-
 local Trove = {}
 
 Trove.bird_data = {}
 
-function Trove.load_folder(path)
+local function load_csv(file_path)
 
-  -- TODO ref Timber Player
-  
-  for i = 1, 3 do
-    Trove.bird_data[i] = {
-      slices = {},
-      name = "None " .. i
-    }
+  local bird = {
+    name = file_path,
+    slices = {},
+    num_slices = 0,
+    min_x = 180,
+    max_x = -180,
+    min_y = 90,
+    max_y = -90
+  }
+
+  local first = true
+
+  for line in io.lines(file_path) do
+
+    -- Skip headers
+    if first then
+      first = false
+
+    else
+
+      -- Split the line
+      local split = {}
+      for val in string.gmatch(line, '([^,]+)') do
+        table.insert(split, val)
+      end
+
+      local week, year = tonumber(split[3]), tonumber(split[4])
+
+      -- Add a new slice if need be
+      if bird.num_slices == 0 or week ~= bird.slices[bird.num_slices].week or year ~= bird.slices[bird.num_slices].year then
+        local slice = {
+          week = week,
+          year = year,
+          points = {}
+        }
+        table.insert(bird.slices, slice)
+        bird.num_slices = bird.num_slices + 1
+      end
+      
+      -- Add point
+      local point = {
+        x = tonumber(split[1]),
+        y = tonumber(split[2])
+      }
+      table.insert(bird.slices[bird.num_slices].points, point)
+
+      -- Store min/max
+      if point.x < bird.min_x then bird.min_x = point.x end
+      if point.x > bird.max_x then bird.max_x = point.x end
+      if point.y < bird.min_y then bird.min_y = point.y end
+      if point.y > bird.max_y then bird.max_y = point.y end
+      
+    end
+  end
+
+  if bird.num_slices > 0 then
+    table.insert(Trove.bird_data, bird)
+    print("Added bird", bird.name)
+  else
+    print("Could not add bird", bird.name)
+  end
+
+end
+
+function Trove.load_folder(folder_path)
+
+  local files = util.scandir(folder_path)
+  for _, v in ipairs(files) do
+
+    if string.sub(v:lower(), -4, -1) == ".csv" then
+      load_csv(folder_path .. v)
+    else
+      print("Skipped", v)
+    end
+
   end
 
 end
--- migration.lua
+++ migration.lua
@@ -11,7 +11,7 @@ local screen_dirty = true
 
 local timeline_clock
 
-local slice_index = 0
+local slice_index = 1
 local bird_data = {}
 
 
@@ -26,10 +26,16 @@ end
 
 local function advance()
   while true do
+
     slice_index = slice_index + 1
+    if slice_index > Trove.bird_data[params:get("selected_bird")].num_slices then
+      slice_index = 1
+    end
+    -- slice_index = 53
+
     screen_dirty = true
-    print("tick", slice_index)
-    clock.sync(1)
+    -- print("tick", slice_index)
+    clock.sync(0.1)
   end
 end
 
@@ -69,14 +75,19 @@ end
 
 function init()
 
-  Trove.load_folder("data")
+  Trove.load_folder(norns.state.path .. "data/")
+
+  local bird_names = {}
+
+  if #Trove.bird_data == 0 then
+    table.insert(bird_names, "No bird data")
+  end
 
   -- Add params
   
   params:add_separator("Migration")
 
-  local bird_names = {}
-  for k, v in ipairs(Trove.bird_data) do
+  for _, v in ipairs(Trove.bird_data) do
     table.insert(bird_names, v.name)
   end
 
@@ -97,15 +108,40 @@ function init()
 end
 
 
+local function draw_map()
+
+  local bird = Trove.bird_data[params:get("selected_bird")]
+
+  -- Scale and center on screen
+  
+  local bird_x_range = bird.max_x - bird.min_x
+  local bird_y_range = bird.max_y - bird.min_y
+
+  local scale = math.min(360 / bird_x_range, 180 / bird_y_range)
+  local x_offset = bird_x_range / 2 + bird.min_x
+  local y_offset = bird_y_range / 2 + bird.min_y
+
+  screen.level(15)
+  for _, p in ipairs(bird.slices[slice_index].points) do
+    screen.rect(util.linlin(-180, 180, 0, 127, (p.x - x_offset) * scale), util.linlin(-90, 90, 63, 0, (p.y - y_offset) * scale), 1, 1)
+    screen.fill()
+  end
+
+end
+
 function redraw()
+  
   screen.clear()
   screen.aa(1)
 
+  draw_map()
+
   screen.level(15)
-  screen.move(10, 16)
-  screen.text(params:string("selected_bird"))
-  screen.move(10, 26)
-  screen.text("Slice " .. slice_index)
+  screen.move(0, 11)
+  local slice = Trove.bird_data[params:get("selected_bird")].slices[slice_index]
+  screen.text(slice.year .. " " .. slice.week)
+  -- screen.text(params:string("selected_bird"))
+  -- screen.move(10, 26)
   screen.fill()
 
   screen.update()
